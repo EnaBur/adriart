d707d44c6f4cb8ca3df3ed3b28945af9474da2d3
EnaBur <ena.buric284@gmail.com>
2024-05-14 14:39:17 +0200

    mobile-view

---
+++ src/components/Hamburger.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Hamburger = () => {
+  return (
+    <div className="hamburger">
+      <FontAwesomeIcon icon={faBars} />
+    </div>
+  );
+};
+export default Hamburger;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,42 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import Hamburger from "./Hamburger";
 
 const Nav = () => {
   const logo = require("../images/Adriart_negativ_bijeli.png");
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
 
   return (
-    <div>
+    <div className="nav">
       <ul>
         <img alt="logo" src={logo}></img>
         <h2>Adriart</h2>
-        <li>
-          <a
-            href="https://www.instagram.com/villa.adriart/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://web.facebook.com/villa.adriart/?_rdc=1&_rdr"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-        </li>
-        <li>
-          <a href="#contact">CONTACT</a>
-        </li>
-        <li>
-          <a href="#gallery">GALLERY</a>
-        </li>
-        <li>
-          <a href="#about">ABOUT</a>
-        </li>
+        <div className="hamburger" onClick={toggleHamburger}>
+          <Hamburger />
+        </div>
+        <div className="nav-mobile">
+          <li className={hamburgerOpen ? "open" : ""}>
+            <a
+              href="https://www.instagram.com/villa.adriart/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+          <li className={hamburgerOpen ? "open" : ""}>
+            <a
+              href="https://web.facebook.com/villa.adriart/?_rdc=1&_rdr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </li>
+          <li className={hamburgerOpen ? "open" : ""}>
+            <a href="#contact">CONTACT</a>
+          </li>
+          <li className={hamburgerOpen ? "open" : ""}>
+            <a href="#gallery">GALLERY</a>
+          </li>
+          <li className={hamburgerOpen ? "open" : ""}>
+            <a href="#about">ABOUT</a>
+          </li>
+        </div>
       </ul>
     </div>
   );
