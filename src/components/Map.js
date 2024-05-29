b7e3349330054c62e789262911f92ae78f7a2003
EnaBur <ena.buric284@gmail.com>
2024-05-29 14:19:39 +0200

    map_error2

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,7 @@ const Map = () => {
       <h2>FIND US HERE</h2>
       <iframe
         title="findUs"
+        className="map-iframe"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2874.452960064844!2d15.693945676387413!3d43.90859543587488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1334d5bdbf0f0679%3A0x5c2637f2bea10575!2sVilla%20Adriart!5e0!3m2!1sen!2shr!4v1715425250064!5m2!1sen!2shr"
       ></iframe>
     </div>
