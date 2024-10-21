222d26555f5f846413df878a56dbbcdfc72379e0
EnaBur <ena.buric284@gmail.com>
2024-10-21 23:50:24 +0200

    colorUpdates

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Map from "./components/Map";
 import Form from "./components/Form";
+//import Video from "./components/Video";
+
 
 function App() {
   const [showForm, setShowForm] = useState(false);
@@ -17,6 +19,7 @@ function App() {
       <About />
       <Gallery />
       <Map />
+      
       <Contact />
       {showForm && <Form setShowForm={setShowForm} />}
       <Footer />
